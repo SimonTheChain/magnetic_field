da0b1154d56e11032fc54bdb5490fcdcf5d6e43d
Simon Lachaine <simon.lachaine@sentiom.com>
2023-03-16 14:57:07 -0400

    Created main file

---
+++ magnetic_field/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+import config
+import data_management
+import preprocessors
+
+
+# import data
+data_management.download_data_files()
+df = data_management.load_datasets()
+
+# preprocess data
+df = preprocessors.impute_missing(df)
+df = preprocessors.tabular_to_numeric(df)
+
+# identify target and "dependant"(?) variables
+df_target = df[config.TARGET_FEATURE]
+df_predictors = df.drop([config.TARGET_FEATURE], axis=1)
+
+# split the dataset in training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    df_predictors, 
+    df_target,
+    train_size=0.8, 
+    test_size=0.2, 
+    random_state=10,
+)
+
+# initialize and train model
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+# make predictions
+preds = model.predict(X_test)
+
+# evaluate model
+error = mean_absolute_error(y_test, preds)
+print("Model error: {}".format(error))
