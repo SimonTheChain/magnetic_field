1d6d386bd6550156569fe5c131c6e0f23c1e2b82
Simon Lachaine <simon.lachaine@sentiom.com>
2023-03-16 16:29:59 -0400

    Added pipeline file

---
+++ magnetic_field/pipeline.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+
+import preprocessors as pp
+
+
+magnetic_field_pipe = Pipeline(
+    [
+        ("Most Frequent Imputer", pp.FrequentImputer()),
+        ("Tabular to Numeric", pp.TabularToNumeric()),
+        ("Regressor", RandomForestRegressor()),
+      ]
+)
